69c081ffcf732301e87f838cb04fe63b2edf4fa3
Michael Albrecht <michael.albrecht@st.ovgu.de>
2018-01-16 02:36:43 +0100

    Added field to save what object the ray hit

--- ray.py
+++ ray.py
@@ -8,12 +8,22 @@ Created on Tue Dec 12 13:26:46 2017
 
 import numpy as np
 
-class Ray :
 
-# when a ray hits an object, the intersecion method will shorten the ray by altering the t parameter
+class Ray :
+    maxRayLength = 10000.0
+
+
     def __init__(self, oa = np.array([0,0,0]), da = np.array([1,0,0])) :
         self.o = oa
         self.d = da
-        self.t = np.float64(10000.0)
+        self.t = np.float64(Ray.maxRayLength)
+        self.firstHitShape = 0 # will be set to the object the ray hit closest
+        return
+
+
+    def hasHitSomething(self) :
+        # when a ray hits an object, the intersection method will shorten the ray by altering the t parameter
+        return self.t < Ray.maxRayLength
+
         
         
