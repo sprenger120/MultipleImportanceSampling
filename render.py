d2dd326a69dcb12ebad090ff9814af54807d34ce
Michael Albrecht <michael.albrecht@st.ovgu.de>
2018-01-17 21:14:38 +0100

    Reduced light levels so they don't drown out the objects

--- render.py
+++ render.py
@@ -15,6 +15,7 @@ from Shapes.sphere import Sphere
 from camera import Camera
 from scene import Scene
 from Shapes.Lights.Lights import SphereLight
+from sys import stdout
 
 
 def createScene() :
@@ -31,7 +32,7 @@ def createScene() :
 
     scene.lights.append(
         SphereLight(np.array([5.0, 0, 3.0]), 3.0, #position, radius
-                    [1, 1, 1], 1000) # light color, light intensity
+                    [1, 1, 1], 2) # light color, light intensity
     )
     
     return scene
@@ -44,8 +45,9 @@ def createCornellBox():
 
 
 def render( res_x, res_y, scene, integrator) :
-    
+    print("\n")
     cam = Camera( res_x, res_y)
+    totalPixels = res_x * res_y
     
     for ix in range( res_x) :
         for iy in range( res_y) :
@@ -56,6 +58,7 @@ def render( res_x, res_y, scene, integrator) :
             # always clip color so that imshow interpretes as rgb 0.0 - 1.0
             # clip color will also check color valididty and raise exception if necessary
             cam.image[ix,iy, :] = util.clipColor(ellval)
+        print("\rRender Progress", np.floor(((ix*res_x + iy) / totalPixels) * 100), "%       ", end='', flush=True)
 
     return cam.image
     
@@ -64,7 +67,7 @@ def render( res_x, res_y, scene, integrator) :
 integrator = MISIntegrator()
 scene = createScene()
 
-im = render( 512, 512, scene, integrator)
+im = render( 256, 256, scene, integrator)
 
 plt.imshow( im)
 #plt.colorbar()
