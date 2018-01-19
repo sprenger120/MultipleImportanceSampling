2c4213621831c1865b0a98ba610fd90ca140c805
Michael Albrecht <michael.albrecht@st.ovgu.de>
2018-01-20 00:00:40 +0100

    Added triangle light source

--- Shapes/Lights/Lights.py
+++ Shapes/Lights/Lights.py
@@ -1,5 +1,6 @@
 from Shapes.Lights.LightBase import LightBase
 from Shapes.sphere import Sphere
+from Shapes.Triangle import Triangle
 
 
 class SphereLight (LightBase, Sphere) :
@@ -7,4 +8,11 @@ class SphereLight (LightBase, Sphere) :
     def __init__(self, pos, r, lightColor, lightIntensity) :
         LightBase.__init__(self, lightColor, lightIntensity)
         Sphere.__init__(self, pos, r, lightColor)
+        return
+
+class TriangleLight(LightBase, Triangle) :
+
+    def __init__(self, v1, v2,v3, lightColor, lightIntensity) :
+        LightBase.__init__(self, lightColor, lightIntensity)
+        Triangle.__init__(self, v1,v2,v3, lightColor)
         return
