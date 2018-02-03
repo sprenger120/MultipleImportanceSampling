a1d70192c0717ad0e3bd4e23c173235189c518a9
Michael Albrecht <sprenger120@gmx.de>
2018-02-03 01:44:30 +0100

    Moved bounding volume to own class in octree

--- Shapes/shape.py
+++ Shapes/shape.py
@@ -6,34 +6,12 @@ Created on Tue Dec 12 13:54:42 2017
 
 @author: lessig
 """
-
-
-"""
-Axis Aligned Bounding Box Coordinates (Global)
-        
-         
-        v1--------v4
-       .|        .|
-      . |       . |
-     .  |      .  |
-    v2--------v3  |
-    |   |     |   |
-    |   |     |   |
-    |   |     |   |
-    |   v5----|---v8
-    |  .      | .
-    |.        |. 
-    v6--------v7
-        
-
-
-"""
-
 import util as util
 
 from abc import ABCMeta
+from Scene.Octree import BoundingVolume
 
-class Shape :
+class Shape(BoundingVolume) :
     __metaclass__ = ABCMeta
     
     objectid = 0
@@ -44,20 +22,14 @@ class Shape :
             raise Exception()
 
         self.color = color
-        self.BBv1 = np.zeros(3)
-        self.BBv2 = np.zeros(3)
-        self.BBv3 = np.zeros(3)
-        self.BBv4 = np.zeros(3)
-        self.BBv5 = np.zeros(3)
-        self.BBv6 = np.zeros(3)
-        self.BBv7 = np.zeros(3)
-        self.BBv8 = np.zeros(3)
-        self.calcAABB()
+
+        BoundingVolume.__init__(self)
 
     def intersect(self, ray):
         raise NotImplementedError()
 
     def calcAABB(self):
         raise NotImplementedError()
+
         
         
